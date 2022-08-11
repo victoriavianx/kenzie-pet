f20db87d856c68fe7eb6208d5c623db9bef8444e
victoriavianx <victoriagarcia963@gmail.com>
2022-08-11 13:42:43 -0300

    feat: post, get, patch routes

--- animals/serializers.py
+++ animals/serializers.py
@@ -1,7 +1,9 @@
-from rest_framework import serializers
-from ..groups.serializers import GroupSerializer
-from ..traits.serializers import TraitSerializer
+from rest_framework import serializers, exceptions
+from groups.serializers import GroupSerializer
+from traits.serializers import TraitSerializer
 from .models import Animal
+from groups.models import Group
+from traits.models import Trait
 
 class AnimalSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
@@ -13,13 +15,25 @@ class AnimalSerializer(serializers.Serializer):
     traits = TraitSerializer(many=True)
 
     def create(self, validated_data: dict):
-        obj, created = Animal.objects.get_or_create(**validated_data, defaults={"group": validated_data["group"], "traits": validated_data["traits"]})
+        group_data = validated_data.pop("group")
+        trait_data = validated_data.pop("traits")
 
-        return obj
+        groupObj, _ = Group.objects.get_or_create(**group_data)
+
+        animal_data = Animal.objects.create(**validated_data, group=groupObj)
+
+        for trait in trait_data:
+            traitObj, _ = Trait.objects.get_or_create(**trait)
+            animal_data.traits.add(traitObj)
+        
+        return animal_data
 
     def update(self, instance: Animal, validated_data: dict):
 
         for key, value in validated_data.items():
+            if key == "group" or key == "traits" or key == "sex":
+                raise exceptions.ValidationError({f"{key}": f"You can not update {key} property."})
+
             setattr(instance, key, value)
 
         instance.save()
--- animals/views.py
+++ animals/views.py
@@ -1,13 +1,35 @@
-from rest_framework.views import APIView, status, Request, Response
+from rest_framework.views import APIView, status, Request, Response, exceptions
 from .serializers import AnimalSerializer
+from .models import Animal
 
 class AnimalView(APIView):
-    def post(self, request: Request):
+    def post(self, request: Request) -> Response:
         serializer = AnimalSerializer(data=request.data)
 
         if not serializer.is_valid():
-            return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         serializer.save()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def get(self, request: Request) -> Response:
+        animals = Animal.objects.all()
+        serializer = AnimalSerializer(animals, many=True)
+
+        return Response(serializer.data)
+
+class AnimalDetailView(APIView):
+    def patch(self, request: Request, animal_id: int) -> Response:
+        try:
+            animal = Animal.objects.get(pk=animal_id)
+            serializer = AnimalSerializer(animal, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+        except Animal.DoesNotExist:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        except exceptions.ValidationError:
+            return Response(serializer.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+        serializer.save()
+
+        return Response(serializer.data)
